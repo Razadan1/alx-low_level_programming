d5c08639fbad9241430597d415ce2d86478dae0c
Razadan1 <danmoleomomayowa@gmail.com>
2023-12-14 09:28:55 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A c program that prints exactly the word.
+ *
+ * Return: returns nothing
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
