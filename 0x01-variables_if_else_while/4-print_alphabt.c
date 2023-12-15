52e6d8cee918774b0ae9889239626fd42bc76e19
Razadan1 <danmoleomomayowa@gmail.com>
2023-12-15 08:50:48 -0800

    updates

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - printthe alphabets in small letter except q and e.
+ *
+ * Return: return nothing
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		if (c != 'e' || c != 'q')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
