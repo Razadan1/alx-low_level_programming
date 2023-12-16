03f95e6ecf8d217a6e9af0b81083970043a6829f
Razadan1 <danmoleomomayowa@gmail.com>
2023-12-15 23:11:34 -0800

    updates

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - print allpossible combination of three digits
+ *
+ * warning - a number must not appear twice
+ *
+ * Return: return nothing
+ */
+int main(void)
+{
+	int c, d, e;
+
+	c = 0;
+	while (c <= 8)
+	{
+		d = c + 1;
+		while (d <= 9)
+		{
+			putchar(c + '0');
+			putchar(d + '0');
+			putchar(e + '0');
+			e = c + 1;
+			if (c != 8 || d != 9 || e != 7)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			e++;
+		d++;
+		}
+	c++;
+	}
+	putchar('\n');
+	return (0);
+}
