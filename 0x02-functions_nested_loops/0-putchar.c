41f4e98cd9bcb2fdd27681db9b40c5c0c4e42e7b
Razadan1 <danmoleomomayowa@gmail.com>
2023-12-19 05:22:28 -0800

    updates

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#ifndef "MAIN.H"
+#define "Main.H"
+
+/**
+ * main - a program that prints _putchar followed by new line
+ *
+ * warning: do not use printf or puts.
+ *
+ * warning: you can only use _putchar.
+ *
+ * Return: return nothing
+ */
+int main(void)
+{
+	_putchar("_putchar");
+	return (0);
+}
