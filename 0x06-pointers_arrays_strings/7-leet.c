76423e46959f9e69b19f5f525658617f529b2e0f
Raza <128757716+Razadan1@users.noreply.github.com>
2024-01-29 16:42:23 +0100

    Create 7-leet.c
    Update

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+/**
+* leet - encodes string to numbers
+* @ptr: character to be transformed
+* Return: return ptr to the encoded value
+*/
+
+char *leet(char *ptr)
+{
+	char *ltr = ptr;
+
+	while (*ltr != '\0')
+	{
+    char c = *ltr;
+
+		if (c >= 'a' && c <= 'z')
+		{
+			if (c == 'a')
+			{
+				*ltr = '4';
+			}
+			else if (c == 'e')
+			{
+				*ltr = '3';
+			}
+			else if (c == 'o')
+			{
+				*ltr = '0';
+			}
+			else if (c == 't')
+			{
+				*ltr = '7';
+			}
+			else if (c == 'l')
+			{
+				*ltr = '1';
+			}
+		}
+		else if (c >= 'A' && c <= 'Z')
+		{
+			if (c == 'A')
+			{
+				*ltr = '4';
+			}
+			else if (c == 'E')
+			{
+				*ltr = '3';
+			}
+			else if (c == 'O')
+			{
+				*ltr = '0';
+			}
+			else if (c == 'T')
+			{
+				*ltr = '7';
+			}
+			else if (c == 'L')
+			{
+				*ltr = '1';
+			}
+		}
+		ltr++;
+	}
+	return (ptr);
+}
