d2f161379a86c6ada9006a6ec64ffff13afdb094
Razadan1 <danmoleomomayowa@gmail.com>
2024-01-29 12:47:52 +0100

    Update

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,37 +7,27 @@
  */
 int _atoi(char *s)
 {
-	int a, b, c, lenght, n, num;
+	int sign = 1;
+	int result = 0;
+	int a;
 
-	b = 0;
-	c = 0;
-	lenght = 0;
-	n = 0;
-	num = 0;
-
-	while (s[lenght] != '\0')
-		lenght++;
-	for (a = 0; a < lenght && n == 0; a++)
+	for (a = 0; s[a] != '\0'; ++a)
 	{
 		if (s[a] == '-')
-			++b;
+		{
+			sign *= -1;
+		}
 
 		if (s[a] >= '0' && s[a] <= '9')
 		{
-			num = s[a] - '0';
-			if (b % 2)
-				num = -num;
-			c = c * 10 + num;
-			n = 1;
+			result = result * 10 + (s[a] - '0');
+		}
 
-			if (s[a + 1] < '0' || s[a + 1] > '9')
-				break;
-
-			n = 0;
+		if (result != 0 && (s[a] < '0' || s[a] > '9'))
+		{
+			break;
 		}
 	}
-	if (n == 0)
-		return (0);
 
-	return (n);
+	return (result * sign);
 }
