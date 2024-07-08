a37f0ca4a785490dd1aa7fec607392793b5479b1
Razadan1 <danmoleomomayowa@gmail.com>
2024-07-08 13:42:04 +0100

    Task 101

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* main - finds and prints the largest prime factor
+* Return: void
+*/
+int main(void)
+{
+	long num = 612852475143;
+
+	int i;
+
+	while (i++ < num / 2)
+	{
+		if (num % i == 0)
+		{
+			num /= 2;
+			continue;
+		}
+
+		for (i = 3; i < num / 2; i += 2)
+		{
+			if (num % i == 0)
+				num /= i;
+		}
+		printf("%ld\n", num);
+		return (0);
+	}
+}
