8197c1a69f49df0edbd38557664a3edfe1dcf703
Razadan1 <danmoleomomayowa@gmail.com>
2024-08-05 10:44:38 +0100

    Task 101

--- 0x07-pointers_arrays_strings/2-main.c
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -6,7 +6,7 @@
 *
 * Return: Always 0.
 */
-int main(void)
+/* int main(void)
 {
 	char *s = "hello";
 
@@ -18,5 +18,23 @@ int main(void)
 	{
 		printf("%s\n", f);
 	}
+	return (0);
+}
+*/
+
+char *_strchr(char *, char);
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *s = "First, solve the problem. Then, write the code.";
+	char *f;
+
+	f = _strchr(s, '\0');
+	printf("%s\n", (f == NULL ? "nil" : f));
 	return (0);
 }
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <cstddef>
 
 /**
 * _strchr - Locates a character in a string
@@ -10,15 +11,16 @@
 char *_strchr(char *s, char c)
 
 {
+	if (c == '\0')
+		return (NULL);
 	while (*s != '\0') /*Declaring WHILE*/
 
 {
 	if (*s == c) /*if s ==  c*/
+	{
+		return (s); /*return s*/
+	}
 
-{
-	return (s); /*return s*/
-
-}
 	s++;
 }
 	return (0);/*return null*/
